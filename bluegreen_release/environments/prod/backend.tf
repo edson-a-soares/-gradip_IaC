911e764aae6c12fd9f6d17aa5da6711e8ce9f617
edson-a-soares <edson.araujo.soares@gmail.com>
2021-02-07 17:15:47 -0300

    Allow terraform manage its workspaces

--- bluegreen_release/environments/prod/backend.tf
+++ bluegreen_release/environments/prod/backend.tf
@@ -4,7 +4,7 @@ terraform {
     region  = "us-east-1"
     profile = "secondary"
     bucket  = "bluegreen.terraform"
-    key     = "releases/terraform.tfstate"
+    key     = "terraform.tfstate"
     shared_credentials_file = "~/.aws/credentials"
   }
 }
