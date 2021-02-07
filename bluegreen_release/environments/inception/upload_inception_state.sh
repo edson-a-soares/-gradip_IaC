a1c120074999d3c5c1a09f7601ccd8b584e42ce3
edson-a-soares <edson.araujo.soares@gmail.com>
2021-02-07 16:41:57 -0300

    Fix script for upload_inception_state.sh manually

--- bluegreen_release/environments/inception/upload_inception_state.sh
+++ bluegreen_release/environments/inception/upload_inception_state.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-aws s3 cp terraform.tfstate s3://bluegreen.terraform/inception --profile secondary
+aws s3 cp terraform.tfstate s3://bluegreen.terraform/inception/terraform.tfstate --profile secondary
